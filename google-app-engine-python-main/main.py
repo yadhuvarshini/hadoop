9c71b7f432d81e642240c60ba25ecf5de7899426
RYV <97048677+yadhuvarshini@users.noreply.github.com>
2023-05-18 22:37:15 -0700

    Add files via upload

---
+++ google-app-engine-python-main/main.py
@@ -0,0 +1,11 @@
+import time
+print("hello world")
+time.sleep(10)    
+# def executeSomething():
+#     #code here
+#     print("Hello world!")
+#     # time.sleep(1)
+    
+# while True:
+    # executeSomething()
+# google-cloud-sdk\bin\dev_appserver.py --enable_host_checking=False "D:\Codeoverflow\google\google app engine"
